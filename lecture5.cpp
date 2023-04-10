c938f189a4317b01b64a4dfe3d6f61e7cb9a85a5
jiksad2 <127714140+jiksad2@users.noreply.github.com>
2023-04-10 13:14:47 +0900

    Add files via upload

---
+++ lecture5.cpp
@@ -0,0 +1,137 @@
+﻿// lecture5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+//#define DEBUG
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+// game_state == 0 일때
+int print_title_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*              지렁이 게임               *" << std::endl;
+    std::cout << "*             (Snake  Bite)              *" << std::endl;
+
+#ifdef DEBUG
+    std::cout << "*            - 디버그 모드 -             *" << std::endl;
+#else
+    std::cout << "*                                        *" << std::endl;
+#endif  
+    std::cout << "*   1. 게임 시작                         *" << std::endl;
+    std::cout << "*   2. 게임 설명                         *" << std::endl;
+    std::cout << "*   3. 게임 랭킹 보기                    *" << std::endl;
+    std::cout << "*   4. 게임 종료 (esc)                   *" << std::endl;
+    std::cout << "******************************************" << std::endl;
+
+    return 0;
+}
+
+void gotoxy(int x, int y) {
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int print_game_screen(int stage_width, int stage_height)
+{
+    gotoxy(15, 15);
+    for (int i = 15; i <= stage_width + 14; i++) {
+        for (int j = 15; j <= stage_height + 14; j++) {
+            if (i == 15 || i == stage_width + 14 || j == 15 || j == stage_height + 14) {
+                gotoxy(i, j);
+                std::cout << "^^";
+            }
+        }
+    }
+    return 0;
+}
+
+// game_state == 2 일때
+int print_introduction_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "타이틀화면으로 돌아갈까요? (Y/N)" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    int stage_width = 0;
+    int stage_height = 0;
+
+    int game_state = 0;
+    int is_game_running = 1;
+
+    while (is_game_running)
+    {
+        char key_input = '0';
+        switch (game_state)
+        {
+        case 0:
+            print_title_screen();
+            key_input = _getch();
+            switch (key_input)
+            {
+            case '1':
+                game_state = 1;
+                break;
+            case '2':
+                game_state = 2;
+                break;
+            case '3':
+                break;
+            case '4':
+                is_game_running = 0;
+                break;
+            case 27:
+                is_game_running = 0;
+                break;
+            default:
+                break;
+            }
+            break;
+        case 1:
+            std::cout << "스테이지 가로 길이 입력:";
+            std::cin >> stage_width;
+            std::cout << "스테이지 세로 길이 입력:";
+            std::cin >> stage_height;
+            print_game_screen(stage_width, stage_height);
+            key_input = _getch();
+        case 2:
+            print_introduction_screen();
+            key_input = _getch();
+            switch (key_input)
+            {
+            case 'y':
+                game_state = 0;
+                break;
+            case 'n':
+                break;
+            default:
+                break;
+            }
+            break;
+
+        default:
+            break;
+        }
+
+
+    }
+
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
